12c92086a90fb2788d4b098b31cddcd0d2361158
Austin-Derrick <aderrickson@colum.edu>
2021-02-11 16:17:49 -0600

    Added player class and Set_Hand function

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,17 @@ class Card{
 
 void Card::intializeCard(const int VAL, string SUIT) { this->value = VAL; this->suit = SUIT;}
 
+class Player{
+    public:
+        string name;
+        Card hand [5];
+        void Set_Hand();
+} ;
+
+void Player::Set_Hand(){
+
+}
+
 int main() {
     Card deck [52];
     for(int i = 0; i < 52; i++){
