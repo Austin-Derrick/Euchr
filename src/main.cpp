8d70cf6bafba10e845c52125d6c121924f2c7fe2
Austin-Derrick <aderrickson@colum.edu>
2021-02-11 16:08:09 -0600

    Added basic card initialization logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include <string>
+using std::string;
 
 class Card{
     public: 
         int value;
-        //string suit;
-        void intializeCard(const int VAL);
+        // char * suit;
+        string suit;
+        void intializeCard(const int VAL, string SUIT);
 };
 
-void Card::intializeCard(const int VAL) { this->value = VAL;}
+void Card::intializeCard(const int VAL, string SUIT) { this->value = VAL; this->suit = SUIT;}
 
 int main() {
     Card deck [52];
     for(int i = 0; i < 52; i++){
-        deck[i].intializeCard(i);
+        deck[i].intializeCard(i, "Hearts");
     }
     for(int i = 0; i < 52; i++){
         std::cout<<deck[i].value<< std::endl;
+        std::cout<<deck[i].suit<< std::endl;    
     }
-}
+}   
