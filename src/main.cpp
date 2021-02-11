d34b649a6bf361f73540800fe9536c657fc7815f
Austin-Derrick <aderrickson@colum.edu>
2021-02-11 15:50:02 -0600

    Added Card class with InitializeCard function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <string>
+
+class Card{
+    public: 
+        int value;
+        //string suit;
+        void intializeCard(const int VAL);
+};
+
+void Card::intializeCard(const int VAL) { this->value = VAL;}
 
 int main() {
-    std::cout<<"Hello World"<< std::endl;
+    Card deck [52];
+    for(int i = 0; i < 52; i++){
+        deck[i].intializeCard(i);
+    }
+    for(int i = 0; i < 52; i++){
+        std::cout<<deck[i].value<< std::endl;
+    }
 }
