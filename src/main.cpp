a314e05ee16c263ba41742e1e784ed4aee1eb0af
Austin-Derrick <Austinderrick15@gmail.com>
2021-02-23 20:00:45 -0600

    Optimized player hand setting and printing

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <algorithm>
 using std::string;
 using std::vector;
-
+    
 enum Rank {NINE = 9, TEN, JACK, QUEEN, KING, ACE, TRUMP};
 enum Suit {SPADES, CLUBS, DIAMONDS, HEARTS};
 
@@ -17,24 +17,47 @@ class Card{
 
 Card::Card( Rank VAL, Suit SUIT) { this->value = VAL; this->suit = SUIT;}
 
-// class Player{
-//     public:
-//         string name;
-//         vector<Card> hand [5];
-//         void Set_Hand();
-// } ;
-
-
-// void Player::Set_Hand(){
-
-// }
-
 struct Deck
 {
     vector <Card> cards;
     int MAX_SIZE = 24;
 };
 
+// Prints a passed through card's value and suit
+void printCard(const Card& card) { std::cout<<card.value << " of " << card.suit <<std::endl; }
+
+class Player{
+    public:
+        string name;
+        vector<Card> hand;
+        Player();
+        void setHand(Deck& deck);
+        void printHand();
+};
+
+Player::Player(){
+    for(int i = 0; i < 6; i++){
+        hand.push_back(Card(Rank(), Suit()));
+    }
+}
+
+void Player::setHand(Deck& deck){
+    hand.clear();
+    for(int i = 0; i < 5; i++){
+        hand.push_back(deck.cards.back());
+        deck.cards.pop_back();
+    }
+}
+
+void Player::printHand(){
+    for (int i = 0; i < hand.size(); i++)
+    {
+        printCard(hand[i]);
+    }
+    
+}
+
+// Organizes a Card vector inside of a deck struct
 void initializeDeck(Deck& deck){
     for(int i = 0; i < 4; i++){
         for(int j = 9; j <= 14; j++){
@@ -44,47 +67,33 @@ void initializeDeck(Deck& deck){
     }
 }
 
-void printCard(const Card& card){
-    std::cout<<card.value << " of " << card.suit <<std::endl;
-}
 
-
+// Prints the Card Vector inside of a Deck Struct
 void printDeck(const Deck& deck){
     for(int i = 0; i < deck.cards.size(); i++){
         printCard(deck.cards[i]);
     }
 }
 
+// Prints the first element of a Card vector from a Deck
+void showTopCard(Deck& deck){
+    printCard(deck.cards.front());
+}
 
-
-// void showTopCard(vector<Card> deck){
-//     std::cout<<deck[0].value << " of " << deck[0].suit << std::endl;
-// }
-
-// void shuffleDeck(vector<Card> deck){
-//     std::random_shuffle(deck.begin(), deck.end());
-// }
+void shuffleDeck(Deck& deck){
+    std::random_shuffle(deck.cards.begin(), deck.cards.end());
+}
 
 int main() {
-    // Player players[4];
     Deck deck;
-
     initializeDeck(deck);
     printDeck(deck);
 
-    // int index = 0;
-    // for(int i = 0; i < 4; i++){
-    //     players[i].name = ("Player " + i);
-    //     for(int j = 0; j < 5; j++){
-    //         players[i].hand->push_back(deck[index]);
-    //         //  std::cout<<players[i].hand[j].value << " of " << players[i].hand[j].suit <<std::endl;
-    //         index++;
-    //     }
-    // }
+    Player players[4];
 
-    // showTopCard(deck);
-    // shuffleDeck(deck);
-    // showTopCard(deck);
-
-}   
-
+    for(int i = 0; i < 4; i++){
+        players[i].setHand(deck);
+        std::cout<<""<<std::endl;
+        players[i].printHand();
+    }
+}
